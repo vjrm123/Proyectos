9db98b33447bf40022393a15bbc28dfa8bb9b417
juan victor <vjrm123@users.noreply.github.com>
2024-09-20 05:21:49 -0600

    actualizando

--- DBP-CCOMP4-2024/FORMULARIO/mostrar_cv.php
+++ DBP-CCOMP4-2024/FORMULARIO/mostrar_cv.php
@@ -18,7 +18,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <meta charset='UTF-8'>
         <meta name='viewport' content='width=device-width, initial-scale=1.0'>
         <title>CV de $nombre</title>
-        <link rel='stylesheet' type='text/css' href='CSS/hoja_cv.css'>
+        <link rel='stylesheet' type='text/css' href='hoja_cv.css'>
     </head>
     <body>
         <div class='cv-container'>
